b8f2746ddd3a8b51da11d950a157600bd6110b57
Anna Carey <acarey@gmail.com>
2013-04-04 09:51:08 -0400

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsbridgeindy::Application.config.secret_token = 'd9e8c9473beb8a5216fd3406e49053ee29bf8e80d4e7f15fb21075b295d7b5848c0f7607ca97cf12e7108c1430f5b723ea2edb1a37cde7c723dd0891f6b878be'
